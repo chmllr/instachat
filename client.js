54f1931e211889aae09dc8dd1e7d01a70e13b8d4
Christian Müller <no@way.click>
2016-11-13 13:47:09 +0100

    adds minimal working UI

--- client.js
+++ client.js
@@ -1,3 +1,27 @@
+let room = 'test'
+let host = 'localhost:12345'
+let socket = new WebSocket('ws://' + host + '/' + room, 'protocolOne');
+
+socket.onmessage = event => {
+    let msg = event.data;
+    console.log("message received", msg)
+    history.innerHTML += msg + '<br/>'
+}
+
+let message, history;
+
+let init = () => {
+    message = document.getElementById('message')
+    history = document.getElementById('history')
+}
+
+let send = () => {
+    socket.send(message.value);
+    message.value = null;
+}
+
+/*
+
 var socket1 = new WebSocket("ws://localhost:12345/test-room", "protocolOne");
 socket1.onmessage = event => console.log("room to guest 1:", event.data);
 socket1.send("anybody here?")
@@ -12,3 +36,5 @@ socket3.send("hi guys")
 
 socket2.send("I leave")
 socket2.close()
+
+*/
