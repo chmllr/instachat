70e67d892a2f8d2bbef621100a7c20a3e81ca715
Christian Müller <no@way.click>
2016-11-13 14:09:03 +0100

    adds usable styling

--- client.js
+++ client.js
@@ -6,6 +6,7 @@ socket.onmessage = event => {
     let msg = event.data;
     console.log("message received", msg)
     history.innerHTML += msg + '<br/>'
+    history.scrollTop = history.scrollHeight;
 }
 
 let message, history;
@@ -15,26 +16,8 @@ let init = () => {
     history = document.getElementById('history')
 }
 
-let send = () => {
+let trySend = event => {
+    if (event.keyCode != 13) return;
     socket.send(message.value);
     message.value = null;
 }
-
-/*
-
-var socket1 = new WebSocket("ws://localhost:12345/test-room", "protocolOne");
-socket1.onmessage = event => console.log("room to guest 1:", event.data);
-socket1.send("anybody here?")
-
-var socket2 = new WebSocket("ws://localhost:12345/test-room", "protocolOne");
-socket2.onmessage = event => console.log("room to guest 2:", event.data);
-socket2.send("hey ho")
-
-var socket3 = new WebSocket("ws://localhost:12345/test-room", "protocolOne");
-socket3.onmessage = event => console.log("room to guest 3:", event.data);
-socket3.send("hi guys")
-
-socket2.send("I leave")
-socket2.close()
-
-*/
